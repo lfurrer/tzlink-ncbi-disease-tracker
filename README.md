7b3e97fc17d00d06aadee5a303ad73aba099f342
Lenz <Lenz.Furrer@gmail.com>
2018-06-01 07:47:32 +0300

    prototype

---
+++ README.md
@@ -0,0 +1,6 @@
+# Result Tracker
+
+Utility for tracking experimental progress over time.
+
+| Date | Changes | Score | Compare | Commit |
+| ---- | ------- | ----- | ------- | ------ |
---
+++ update.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+# Author: Lenz Furrer, 2018
+
+
+'''
+Add a new set of results to the tracker.
+'''
+
+
+import re
+import os
+import sys
+import time
+import argparse
+import subprocess as sp
+
+
+HERE = os.path.dirname(__file__)
+
+BRANCH = 'master'
+REMOTE = 'origin'
+REMOTE_URL = 'https://github.com/USER/PROJECT'
+
+
+def main():
+    '''
+    Run as script: read results from STDIN.
+    '''
+    ap = argparse.ArgumentParser(description=__doc__)
+    ap.add_argument(
+        '-p', '--pull', action='store_true',
+        help='pull from the remote tracker repo before updating')
+    ap.add_argument(
+        '-u', '--push', action='store_true',
+        help='push to the remote tracker repo after updating')
+    args = ap.parse_args()
+    run(sys.stdin.read(), args.pull, args.push)
+
+
+def run(summary, pull=False, push=False):
+    '''
+    Process the result summary and update the repo.
+    '''
+    if pull or push:
+        _git('pull', REMOTE, BRANCH)
+    hash_, msg, results, conf = _parse(summary)
+    msg = msg.split('\n')[0]
+    score = _main_score(results)
+    _add_data(msg, ['results', 'config'], [results, conf])
+    _update_table('README.md', hash_, msg, score)
+    if push:
+        _git('push', REMOTE, BRANCH)
+
+
+def _parse(summary):
+    m = re.match(r'''
+        \#\ Commit\ hash: (.+?)
+        \#\ Commit\ message: (.+?)
+        \#\ Results: (.+?)
+        \#\ Configuration: (.+)
+        ''', summary, re.DOTALL | re.VERBOSE)
+    return [hunk.strip() for hunk in m.groups()]
+
+
+def _main_score(results):
+    acc = float(results.split()[1])
+    return '{:.4}'.format(acc)
+
+
+def _add_data(msg, filenames, contents):
+    for fn, content in zip(filenames, contents):
+        with open(os.path.join(HERE, fn), 'w', encoding='utf8') as f:
+            f.write(content)
+            f.write('\n')
+    _git('add', *filenames)
+    _git('commit', '-m', '{} (data)'.format(msg), '--allow-empty')
+
+
+def _update_table(filename, hash_, msg, score):
+    date = time.strftime('%Y-%m-%d %H:%M:%S')
+    local_link = '[diff](../../commit/{})'.format(_get_hash())
+    remote_link = '[original]({}/commit/{})'.format(REMOTE_URL, hash_)
+    row = ' | '.join([date, msg, score, local_link, remote_link]) + '\n'
+    with open(filename, 'a', encoding='utf8') as f:
+        f.write(row)
+    _git('add', filename)
+    _git('commit', '-m', '{} (table)'.format(msg))
+
+
+def _git(*args):
+    print('git', *args)
+    sp.run(['git', *args], check=True, cwd=HERE)
+
+
+def _get_hash():
+    args = ['git', 'log', '-1', '--pretty=%H']
+    compl = sp.run(args, stdout=sp.PIPE, check=True, cwd=HERE)
+    return compl.stdout.decode('utf8').strip()
+
+
+if __name__ == '__main__':
+    main()
